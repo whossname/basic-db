5f4eb18bbc35633e5b935967874e9ee77163db71
whossname <tyson.buzza@gmail.com>
2019-05-02 17:28:24 +0800

    basic repl

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,44 @@
+extern crate rustyline;
+
+use rustyline::error::ReadlineError;
+use rustyline::Editor;
+
 fn main() {
-    println!("Hello, world!");
+    // `()` can be used when no completer is required
+    let mut rl = Editor::<()>::new();
+    if rl.load_history("history.txt").is_err() {
+        println!("No previous history.");
+    }
+    loop {
+        let readline = rl.readline(">> ");
+        match readline {
+            Ok(cmd) => {
+                run_cmd(cmd.clone());
+                rl.add_history_entry(cmd);
+            }
+            Err(ReadlineError::Interrupted) => {
+                println!("CTRL-C");
+                break;
+            }
+            Err(ReadlineError::Eof) => {
+                println!("CTRL-D");
+                break;
+            }
+            Err(err) => {
+                println!("Error: {:?}", err);
+                break;
+            }
+        }
+    }
+    rl.save_history("history.txt").unwrap();
+}
+
+fn run_cmd(cmd: String) {
+    match cmd.as_ref() {
+        "" => {
+        },
+        _ => {
+            println!("Unknown command: {}\n", cmd.clone())
+        },
+    }
 }

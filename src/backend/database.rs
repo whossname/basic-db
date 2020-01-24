5a9dc0f7e87ebe1d78c391293f88c4b9b1736dac
whossname <tyson.buzza@gmail.com>
2020-01-24 15:03:05 +0800

    describe table logic complete
    has bugs

--- src/backend/database.rs
+++ src/backend/database.rs
@@ -26,14 +26,16 @@ pub struct Database {
     pub file: File,
 }
 
+#[derive(Debug)]
 pub enum Column {
+    Null(),
     Integer(i128),
     Real(f64),
     Blob(Vec<u8>),
     Text(String),
 }
 
-#[derive(serde::Serialize, serde::Deserialize, Debug)]
+#[derive(serde::Serialize, serde::Deserialize, Debug, Clone)]
 #[serde(tag = "t", content = "c")]
 #[repr(u8)]
 pub enum ColumnType {
@@ -65,6 +67,74 @@ impl Database {
             10 => unimplemented!("index_leaf"),
             13 => page::table_leaf::read_page(page, page_header_start),
             _ => panic!("Unknown page type: {}", page_type),
+        }
+    }
+
+    pub fn describe_table(
+        &mut self,
+        table_name: String,
+    ) -> Result<Vec<Vec<Column>>, Box<dyn error::Error>> {
+        let record_filter = |row: Vec<Column>| match &row[1] {
+            Column::Text(row_table_name) => *row_table_name == table_name,
+            _ => false,
+        };
+
+        let column_filter = |row| row;
+
+        self.select_records(1, record_filter, column_filter)
+    }
+
+    pub fn select_records<RecF, ColF>(
+        &mut self,
+        page_number: u32,
+        record_filter: RecF,
+        column_filter: ColF,
+    ) -> Result<Vec<Vec<Column>>, Box<dyn error::Error>>
+    where
+        RecF: Fn(Vec<Column>) -> bool,
+        ColF: Fn(Vec<Column>) -> Vec<Column>,
+    {
+        let page = self.read_page(page_number);
+
+        match page {
+            Ok(Page {
+                page: page_content,
+                page_type: PageType::TableLeaf(leaf),
+            }) => {
+                let mut records: Vec<Vec<Column>> = Vec::new();
+                let mut cell_pointer_start = 8;
+                if page_number == 1 {
+                    cell_pointer_start = 108;
+                }
+
+                for cell_count in 0..leaf.cell_count {
+                    let cell_pointer_index = (cell_pointer_start + 2 * cell_count) as usize;
+                    let slice = &page_content[cell_pointer_index..cell_pointer_index + 2];
+                    let mut cell_pointer: u16 = serialise::to_integer(slice)?;
+
+                    // TODO
+
+                    // fetch and decode the record
+                    let record = fetch_record(&mut (cell_pointer as usize), &page_content);
+                    // apply record_filter and column_filter to each record_filter
+                    records.push(record);
+                }
+                // return records
+
+                Ok(records)
+            }
+
+            Ok(Page {
+                page: page_content,
+                page_type: PageType::TableInterior(interior),
+            }) => {
+                // for each page
+                // recursively call self
+                // return combined results
+
+                panic!("Not implemented")
+            }
+            _ => panic!("Not implemented"),
         }
     }
 
@@ -101,7 +171,6 @@ impl Database {
                 page: mut page_content,
                 page_type: PageType::TableLeaf(leaf),
             }) => {
-
                 // check if there is enough space
                 // if not, do we need to split the leaf or add an overflow page?
 
@@ -172,10 +241,90 @@ impl Database {
     }
 }
 
+fn fetch_record(cell_pointer: &mut usize, page_content: &Vec<u8>) -> Vec<Column> {
+    let cell_start = cell_pointer.clone();
+    let header_size = read_varint(cell_pointer, page_content);
+    let header_end = header_size as usize + cell_start;
+    let mut serial_types: Vec<u64> = Vec::new();
+    while *cell_pointer < header_end {
+        let serial_type = read_varint(cell_pointer, page_content);
+        serial_types.push(serial_type);
+    }
+
+    let mut record: Vec<Column> = Vec::new();
+    for serial_type in serial_types {
+        let column = read_column(serial_type, cell_pointer, page_content);
+        record.push(column);
+    }
+
+    record
+}
+
+fn read_column(serial_type: u64, cell_pointer: &mut usize, page_content: &Vec<u8>) -> Column {
+    match serial_type {
+        0 => Column::Null(),
+        1 => read_integer_column(cell_pointer, page_content, 1),
+        2 => read_integer_column(cell_pointer, page_content, 2),
+        3 => read_integer_column(cell_pointer, page_content, 3),
+        4 => read_integer_column(cell_pointer, page_content, 4),
+        5 => read_integer_column(cell_pointer, page_content, 6),
+        6 => read_integer_column(cell_pointer, page_content, 8),
+        7 => {
+            let mut bytes = [0; 8];
+            for i in 0..8 {
+                bytes[i] = page_content[*cell_pointer + i];
+            }
+
+            let val = f64::from_be_bytes(bytes);
+            *cell_pointer += 8;
+            Column::Real(val)
+        }
+        8 => Column::Integer(0),
+        9 => Column::Integer(1),
+        x if x % 2 == 0 => {
+            let len = (x - 12) / 2;
+            let bytes = read_bytes(cell_pointer, page_content, len as usize);
+            Column::Blob(bytes)
+        }
+        x => {
+            let len = (x - 13) / 2;
+            let bytes = read_bytes(cell_pointer, page_content, len as usize);
+            let text = String::from_utf8(bytes).expect("text stored incorrectly");
+            Column::Text(text)
+        }
+    }
+}
+
+fn read_bytes(cell_pointer: &mut usize, page_content: &Vec<u8>, len: usize) -> Vec<u8> {
+    let end = *cell_pointer + len;
+    let mut bytes = Vec::new();
+
+    while *cell_pointer < end {
+        let byte = page_content[*cell_pointer as usize];
+        bytes.push(byte);
+        *cell_pointer += 1;
+    }
+    bytes
+}
+
+fn read_integer_column(cell_pointer: &mut usize, page_content: &Vec<u8>, size: usize) -> Column {
+    let int_end = *cell_pointer + size;
+    let mut val = 0;
+
+    while *cell_pointer < int_end {
+        val = val << 8;
+        val = val + page_content[*cell_pointer as usize] as i128;
+        *cell_pointer += 1;
+    }
+
+    Column::Integer(val)
+}
+
 pub fn create_record(row: Vec<Column>) -> Vec<u8> {
     let data: Vec<(u64, Vec<u8>)> = row
         .into_iter()
         .map(|column| match column {
+            Column::Null() => (0, Vec::new()),
             Column::Integer(int) => serialise_record_integer(int),
             Column::Real(real) => (7, real.to_be_bytes().to_vec()),
             Column::Text(text) => {
@@ -232,6 +381,31 @@ fn build_varint(int: u64) -> Vec<u8> {
     varint
 }
 
+fn read_varint(cell_pointer: &mut usize, page_content: &Vec<u8>) -> u64 {
+    let mut varint = 0u64;
+    let mut byte_count = 0;
+
+    loop {
+        let byte = page_content[*cell_pointer];
+        *cell_pointer += 1;
+        byte_count += 1;
+
+        if byte_count == 9 {
+            varint = varint << 8;
+            varint = varint + byte as u64;
+            return varint;
+        }
+
+        varint = varint << 7;
+        varint = varint + (byte & 0x7F) as u64;
+
+        let finished_flag = (byte & 0x80) as u8;
+        if finished_flag == 0x80 {
+            return varint;
+        }
+    }
+}
+
 // max length of a varint is 9 bytes
 fn varint_length(int: i128) -> u8 {
     let mut leading_bits = int.leading_zeros();
@@ -245,7 +419,6 @@ fn serialise_record_integer(int: i128) -> (u64, Vec<u8>) {
     match int {
         0 => return (8, Vec::new()),
         1 => return (9, Vec::new()),
-        -1 => return (1, vec![255]),
         _ => (),
     }
 
@@ -382,6 +555,12 @@ mod tests {
             ("name string".to_string(), ColumnType::Text),
         ];
 
-        database.create_table(table_name, columns);
+        database.create_table(table_name.clone(), columns.clone());
+        let columns_wrapper = database.describe_table(table_name).unwrap();
+        let columns_out = columns_wrapper.first();
+        println!("{:?}", columns);
+        println!("{:?}", columns_out);
+        // assert_eq!(, columns);
+        panic!("oops")
     }
 }
